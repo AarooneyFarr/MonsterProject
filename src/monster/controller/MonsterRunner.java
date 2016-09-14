1f0e660036427baec6b8457e8ae9b07fa47acae1
AarooneyFarr <Aarooney.Farr@gmail.com>
2016-09-14 08:12:51 -0600

    Initial setup 2

--- src/monster/controller/MonsterRunner.java
+++ src/monster/controller/MonsterRunner.java
@@ -4,6 +4,7 @@ public class MonsterRunner
 {
 	public static void main(String[] args)
 	{
-
+		MonsterController firstMonsterController = new MonsterController();
+		firstMonsterController.start();
 	}
 }

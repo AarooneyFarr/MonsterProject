0bf4d658ca063c0b4529ce98028ca691cf33896a
AarooneyFarr <Aarooney.Farr@gmail.com>
2016-09-14 08:19:17 -0600

    added parameters on controller for data members

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -2,6 +2,17 @@ package monster.controller;
 
 public class MonsterController
 {
+
+	public MonsterController(String name, double legCount, boolean bellybuttonPresent, int noses, int arms, int eyes)
+	{
+		this.name = name;
+		this.legCount = legCount;
+		this.bellybuttonPresent = bellybuttonPresent;
+		this.noses = noses;
+		this.arms = arms;
+		this.eyes = eyes;
+	}
+
 	public void start()
 	{
 
--- src/monster/controller/MonsterRunner.java
+++ src/monster/controller/MonsterRunner.java
@@ -4,7 +4,7 @@ public class MonsterRunner
 {
 	public static void main(String[] args)
 	{
-		MonsterController firstMonsterController = new MonsterController();
-		firstMonsterController.start();
+		MonsterController monsterAppController = new MonsterController();
+		monsterAppController.start();
 	}
 }

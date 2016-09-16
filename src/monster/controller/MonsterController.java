35cd166a45dc1296d366b234e0e951fb6cf3ca53
AarooneyFarr <Aarooney.Farr@gmail.com>
2016-09-16 08:45:27 -0600

    imported Scanner

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,16 +1,18 @@
 package monster.controller;
 
 import monster.model.MarshmallowMonster;
+import java.util.Scanner;
 
 public class MonsterController
 {
 
 	private MarshmallowMonster flirp;
+	private Scanner inputReader;
 
 	public MonsterController()
 	{
 		flirp = new MarshmallowMonster("Flirp", 2, false, 1, 2, 2);
-
+		inputReader = new Scanner(System.in);
 		/*
 		 * name = "Flirp" legCount = 2 Belly button = false noses = 1 eyes = 2
 		 */
@@ -18,6 +20,7 @@ public class MonsterController
 
 	public void start()
 	{
+		nameChanger();
 		System.out.println("My monster's name is " + flirp.getName());
 		if (flirp.getHasBellyButton() == true)
 		{
@@ -28,5 +31,15 @@ public class MonsterController
 			System.out.println("My monster has no bellybutton.");
 
 		}
+		
+	}
+	
+	public void nameChanger(){
+		System.out.println("What should the name of our monster be?");
+		String name = inputReader.nextLine();
+		System.out.println("The old name is: " + flirp.getName() + " The new name is: " + name);
+		flirp.setName(name);
+		inputReader.nextLine();
+		
 	}
 }
